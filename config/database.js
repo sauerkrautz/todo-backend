d5766db5a622187b72e834a16a304073b21b8536
Krausweln <rizqimaulana442@gmail.com>
2022-09-26 09:11:35 +0700

    change

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 const db = new Sequelize("todo_list", "root", "", {
-  host: "127.0.0.1",
+  host: "https://sauerkrautz-todo-backend.herokuapp.com",
   dialect: "mysql",
 });
 
